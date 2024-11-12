5927875bb46d4f8c414d57ba2ecac1f2708be44d
Sebastian Jennen <sj@imagearts.de>
2024-11-12 11:47:53 +0100

    Update Snap7.py - fix access

--- Snap7.py
+++ Snap7.py
@@ -177,8 +177,8 @@ class Snap7(Device, metaclass=DeviceMeta):
     def read_dynamic_attr(self, attr):
         name = attr.get_name()
         # value = self.dynamicAttributes[name].value TODO: once cron based implementation done use the pure value again
-        register_parts = self.dynamicAttributes[name].register_parts
-        variableType = self.dynamicAttributes[name].variableType
+        register_parts = self.dynamicAttributes[name]["register_parts"]
+        variableType = self.dynamicAttributes[name]["variableType"]
         customLength = 0
         if(variableType == CmdArgType.DevString):
             customLength = 254 # see also https://python-snap7.readthedocs.io/en/stable/API/util.html#snap7.util.set_string
@@ -191,14 +191,14 @@ class Snap7(Device, metaclass=DeviceMeta):
     def write_dynamic_attr(self, attr):
         value = str(attr.get_write_value())
         name = attr.get_name()
-        self.dynamicAttributes[name].value = value
+        self.dynamicAttributes[name]["value"] = value
         self.publish(name)
 
     @command(dtype_in=[str])
     def publish(self, name):
         value = self.dynamicAttributes[name].value
-        register_parts = self.dynamicAttributes[name].register_parts
-        variableType = self.dynamicAttributes[name].variableType
+        register_parts = self.dynamicAttributes[name]["register_parts"]
+        variableType = self.dynamicAttributes[name]["variableType"]
         self.info_stream("Publish variable " + str(name) + ": " + str(value))
         data = self.variable_to_bytedata(value, variableType)
         self.write_byte_to_area_offset_size(register_parts.area, register_parts.subarea, register_parts.offset, data)
