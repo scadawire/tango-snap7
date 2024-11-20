f429a58a4c09aa7c483c72c2e1f6f2a110fd2fec
Sebastian Jennen <sj@imagearts.de>
2024-11-20 12:46:16 +0100

    Update Snap7.py - possible strong memory fix

--- Snap7.py
+++ Snap7.py
@@ -162,7 +162,11 @@ class Snap7(Device, metaclass=DeviceMeta):
         elif(variableType == CmdArgType.DevBoolean):
             return snap7.util.get_bool(data, offset, suboffset)
         elif(variableType == CmdArgType.DevString):
-            return snap7.util.get_string(data, offset)
+            out = return snap7.util.get_string(data, offset)
+            out = out.rstrip('\x00')
+            if not out.isprintable():
+                raise ValueError("String contains non-printable characters.")
+            return out
         else:
             raise Exception("unsupported variable type " + variableType)
     
