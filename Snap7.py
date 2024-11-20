8b336a2931dbe9c1c180a3ce7bfb0cddd61bf8af
Sebastian Jennen <sj@imagearts.de>
2024-11-20 12:47:41 +0100

    Update Snap7.py - another possible string fix

--- Snap7.py
+++ Snap7.py
@@ -162,7 +162,7 @@ class Snap7(Device, metaclass=DeviceMeta):
         elif(variableType == CmdArgType.DevBoolean):
             return snap7.util.get_bool(data, offset, suboffset)
         elif(variableType == CmdArgType.DevString):
-            out = return snap7.util.get_string(data, offset)
+            out = snap7.util.get_string(data, offset)
             out = out.rstrip('\x00')
             if not out.isprintable():
                 raise ValueError("String contains non-printable characters.")
